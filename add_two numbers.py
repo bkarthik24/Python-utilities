ff6e28bacd8bee06ad095a760cfa4dbaa49f8949
Karthik <karthik24051992@gmail.com>
2022-09-03 13:44:46 -0700

    Add two number
    Take two numbers from user and add them

---
+++ add_two numbers.py
@@ -0,0 +1,7 @@
+def add(x,y):
+    return x+y 
+first_number = input("Insert first number : ")
+second_number = input("Insert second number :")
+int_first_number = int(first_number)
+int_second_number = int(second_number)
+print(add(int_first_number,int_second_number))
